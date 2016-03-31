d508b5221129e78c4c759107e4d140dcedd2e15a
Your Name <you@example.com>
2016-03-31 20:49:53 +0800

    remove npm and node requirement

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get install -y libjpeg-dev zlib1g-dev nodejs-legacy npm python-pip
+sudo apt-get install -y libjpeg-dev zlib1g-dev python-pip
 sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
-sudo npm -g install pageres-cli
 sudo apt-get install -y espeak
 sudo pip install -r requirements.txt
