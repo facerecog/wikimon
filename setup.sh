ddf00ae0bcd241a206350deb2efc57dc98741b00
Facerecog Asia & 26 Factorial <muhd.amrullah@facerecog.asia>
2016-04-02 04:01:03 +0800

    Update setup.sh
    Added necessary installation of npm and nodejs v5.10.0
    Added necessary installation of pageres-cli

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-sudo apt-get install -y libjpeg-dev zlib1g-dev python-pip
+sudo apt-get install -y libjpeg-dev zlib1g-dev nodejs-legacy npm python-pip
 sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
 sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
+sudo npm -g install pageres-cli
 sudo apt-get install -y espeak
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo ln -sf /usr/local/n/versions/node/v5.10.0/bin/node /usr/bin/node
 sudo pip install -r requirements.txt
