59b3dd2f32adf0745e531473a9bf150e422b0786
Your Name <you@example.com>
2016-04-13 17:09:40 +0800

    removed prompts

--- install.py
+++ install.py
@@ -22,36 +22,36 @@ if __name__ == "__main__":
                 if os.path.isfile("/etc/apt/sources.list"):
                     subprocess.Popen("bash setup.sh", shell=True).wait()
 
-                    # insert the phone number and pw into config.py
+                    # # insert the phone number and pw into config.py
 
-                    print "Please follow the setup instructions at https://github.com/tgalal/yowsup"
-                    print "Please enter the phone number you used:",
-                    phone = raw_input()
-                    password = getpass.getpass(prompt="Please enter the password you received upon registration:")
+                    # print "Please follow the setup instructions at https://github.com/tgalal/yowsup"
+                    # print "Please enter the phone number you used:",
+                    # phone = raw_input()
+                    # password = getpass.getpass(prompt="Please enter the password you received upon registration:")
 
-                    config_file = open("src/config.py", "r")
-                    lines = config_file.readlines()
-                    config_file.close()
+                    # config_file = open("src/config.py", "r")
+                    # lines = config_file.readlines()
+                    # config_file.close()
 
 
-                    login_line = 0
-                    pw_line = 0
+                    # login_line = 0
+                    # pw_line = 0
 
-                    i = 0
-                    for line in lines:
-                        if line.startswith('    "WHATSAPP_LOGIN":'):
-                            login_line = i
-                        elif line.startswith('    "WHATSAPP_PW":'):
-                            pw_line = i
+                    # i = 0
+                    # for line in lines:
+                        # if line.startswith('    "WHATSAPP_LOGIN":'):
+                            # login_line = i
+                        # elif line.startswith('    "WHATSAPP_PW":'):
+                            # pw_line = i
 
-                        i += 1
+                        # i += 1
 
-                    lines[login_line] = '    "WHATSAPP_LOGIN": "' + phone + '",\n'
-                    lines[pw_line] = '    "WHATSAPP_PW": "' + password + '",\n'
+                    # lines[login_line] = '    "WHATSAPP_LOGIN": "' + phone + '",\n'
+                    # lines[pw_line] = '    "WHATSAPP_PW": "' + password + '",\n'
 
-                    config_file = open("src/config.py", "w")
-                    config_file.write("".join(lines))
-                    config_file.close()
+                    # config_file = open("src/config.py", "w")
+                    # config_file.write("".join(lines))
+                    # config_file.close()
 
 
             # if index is out of range then flag options
